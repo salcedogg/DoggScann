c9a85572d0f87eac41beda9e83082deca699ff2f
salcedogg <63917314+salcedogg@users.noreply.github.com>
2023-03-22 18:28:24 -0500

    NewUtilites
    Adding multi threading to scan multiple ports at once
    Adding a progress bar to track the progress of the scan
    Adding a dictionary of commonly used ports to scan

--- DoggScann.py
+++ DoggScann.py
@@ -1,19 +1,56 @@
-import socket  # Importing socket module
+import socket
+import threading
+from tqdm import tqdm
 
-target_host = input("Enter the target host: ")  # User input of target host
-target_ip = socket.gethostbyname(target_host)  # Get IP address of target host
+# Define the target host
+target_host = input("Enter the target host: ")
+
+# Get the IP address of the target host
+try:
+    target_ip = socket.gethostbyname(target_host)
+except socket.gaierror:
+    print("Invalid hostname. Exiting...")
+    exit()
 
 # Print target information
-print(f"Scanning target {target_host} at IP address {target_ip}")
+print(f"Scanning target {target_host} at IP address {target_ip}\n")
 
-# Loop through ports from 1 to 1024
-for port in range(1, 1025):
+# Define a dictionary of commonly used ports to scan
+common_ports = {
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    443: "HTTPS",
+    3306: "MySQL",
+    5432: "PostgreSQL",
+}
+
+# Loop through the common ports and scan them
+print("Scanning commonly used ports...\n")
+for port, service in common_ports.items():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP socket object
-    # Set the timeout for each connection attempt
-    sock.settimeout(0.5)
-    # Attempt to connect to the port on the target
-    result = sock.connect_ex((target_ip, port))
+    sock.settimeout(0.5)  # Set the timeout for each connection attempt
+    result = sock.connect_ex((target_ip, port))  # Attempt to connect to the port on the target
     if result == 0:
-        # If the connection is successful, print that the port is open
+        print(f"Port {port} ({service}): Open")
+    sock.close()
+
+# Define a function to scan a single port
+def scan_port(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP socket object
+    sock.settimeout(0.5)  # Set the timeout for each connection attempt
+    result = sock.connect_ex((target_ip, port))  # Attempt to connect to the port on the target
+    if result == 0:
         print(f"Port {port}: Open")
-    sock.close()  # Close the socket object
+    sock.close()
+
+# Loop through ports from 1 to 1024 and start a new thread for each port
+print("\nScanning all ports...\n")
+for port in tqdm(range(1, 1025)):
+    thread = threading.Thread(target=scan_port, args=[port])
+    thread.start()
