a51b3019d3855cf4642c82de8bd5d6db92d6538e
salcedogg <63917314+salcedogg@users.noreply.github.com>
2023-04-02 19:00:42 -0500

    Adding improvements
    The improved code for the port scanner includes several changes. First, the argparse module was used to pass the target host as an argument in the command line instead of using the input() function. Second, a separate function was created for port scanning to make the code more modular and readable. Third, the threading module was used to scan multiple ports simultaneously, making the scanning process faster. Fourth, exception handling was added to handle any errors that might occur during the scanning process. Finally, the improved code runs within a try-except block to handle the keyboard exception (KeyboardInterrupt) to allow the user to safely stop the scanning process.

--- DoggScann.py
+++ DoggScann.py
@@ -1,19 +1,39 @@
-import socket  # Importing socket module
+import argparse
+import socket
+import threading
 
-target_host = input("Enter the target host: ")  # User input of target host
-target_ip = socket.gethostbyname(target_host)  # Get IP address of target host
-
-# Print target information
-print(f"Scanning target {target_host} at IP address {target_ip}")
-
-# Loop through ports from 1 to 1024
-for port in range(1, 1025):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP socket object
-    # Set the timeout for each connection attempt
+def scan_port(target_ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(0.5)
-    # Attempt to connect to the port on the target
     result = sock.connect_ex((target_ip, port))
-    if result == 0:
-        # If the connection is successful, print that the port is open
+    if result == 0: 
         print(f"Port {port}: Open")
-    sock.close()  # Close the socket object
+    sock.close()
+
+def main():
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", help="Target host to scan")
+    args = parser.parse_args()
+
+    target_host = args.host
+    target_ip = socket.gethostbyname(target_host)
+    print(f"Scanning target {target_host} at IP address {target_ip}")
+
+    # Scan ports using threads
+    threads = []
+    for port in range(1, 1025):
+        t = threading.Thread(target=scan_port, args=(target_ip, port))
+        threads.append(t)
+        t.start()
+
+    # Wait for all threads to finish
+    for t in threads:
+        t.join()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nExiting...")
+
